de35a349c091c18a7ea612d8159a5830fef5cb80
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2015-07-30 09:11:44 +1000

    Update for latest oct2py interface.

--- pywrapper.py
+++ pywrapper.py
@@ -10,12 +10,12 @@ import oct2py as op
 
 def solvedbo(u_q):
     oct_session = op.Oct2Py()
-    oct_session.addpath(os.path.join(os.path.expanduser('~'), 'work/github/phenology-two-trait-migratory-bird'))
+    oct_session.addpath(os.path.abspath(os.path.curdir))
 
-    oct_session.run('[x_cV, yzV, nV, z_n] = solvedbowrapper(%f)' % u_q)
+    oct_session.eval('[x_cV, yzV, nV, z_n] = solvedbowrapper(%f)' % u_q)
 
-    return { 'x_cV': oct_session.get('x_cV'),
-             'yzV':  oct_session.get('yzV'),
-             'nV':   oct_session.get('nV'),
-             'z_n':  oct_session.get('z_n'),
+    return { 'x_cV': oct_session.pull('x_cV'),
+             'yzV':  oct_session.pull('yzV'),
+             'nV':   oct_session.pull('nV'),
+             'z_n':  oct_session.pull('z_n'),
            }
